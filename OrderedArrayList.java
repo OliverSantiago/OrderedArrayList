70b4d243c2644893a3a07bfcc1c9b8f3df8b5074
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-10 15:46:57 -0500

    Intial making and constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList(){
+    super();
+  }
+
+  public OrderedArrayList(int Capacity){
+    super(Capacity);
+  }
+}
