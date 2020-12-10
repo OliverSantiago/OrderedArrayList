6e609dca442ecca50c99ea227fefa386f7b396a3
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-10 15:29:45 -0500

    More tests

--- Tester.java
+++ Tester.java
@@ -3,7 +3,15 @@ public class Tester{
   public static void main(String[]args){
     NoNullArrayList<String> first = new NoNullArrayList<String>();
     NoNullArrayList<String> second = new NoNullArrayList<String>(5);
-    System.out.println(first.toString());
+    second.add("one");
+    second.add("two");
+    second.add("three");
+    second.add("four");
+    // System.out.println(first.toString());
+    // System.out.println(second.toString());
+    second.set(2,"set");
     System.out.println(second.toString());
+    //second.set(2,null);
+
   }
 }
