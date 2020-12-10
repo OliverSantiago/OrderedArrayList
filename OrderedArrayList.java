4f5167c2e347b9186b662f23142ba4ee1106b22a
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-10 16:17:15 -0500

    Second add

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -13,6 +13,10 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     return true;
   }
 
+  public void add(int index, T element){
+    super.add(FindIndex(this, element),element);
+  }
+
   private int FindIndex(OrderedArrayList<T> currentlist, T element){
     for (int i = 0; i < currentlist.size(); i++){
       if (currentlist.get(i).compareTo(element)==1||currentlist.get(i).compareTo(element)==0){
