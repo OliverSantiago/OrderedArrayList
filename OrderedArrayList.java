238efc52655d768cd3590e20875ac7a58003f391
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-10 16:12:02 -0500

    Helper function and first add

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,10 +1,24 @@
 import java.util.ArrayList;
-OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
   public OrderedArrayList(){
     super();
   }
 
   public OrderedArrayList(int Capacity){
     super(Capacity);
+  }
+
+  public boolean add(T element){
+    super.add(FindIndex(this, element),element);
+    return true;
+  }
+
+  private int FindIndex(OrderedArrayList<T> currentlist, T element){
+    for (int i = 0; i < currentlist.size(); i++){
+      if (currentlist.get(i).compareTo(element)==1||currentlist.get(i).compareTo(element)==0){
+        return i;
+      }
+    }
+    return currentlist.size();
   }
 }
