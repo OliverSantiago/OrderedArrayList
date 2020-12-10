05a53db52dabe8d2f3c58300f5c232ddb2330240
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-10 16:11:41 -0500

    Ordered Array Test for first add

--- Tester.java
+++ Tester.java
@@ -3,6 +3,7 @@ public class Tester{
   public static void main(String[]args){
     NoNullArrayList<String> first = new NoNullArrayList<String>();
     NoNullArrayList<String> second = new NoNullArrayList<String>(5);
+    OrderedArrayList<String> third = new OrderedArrayList<String>();
     second.add("one");
     second.add("two");
     second.add("three");
@@ -10,13 +11,18 @@ public class Tester{
     // System.out.println(first.toString());
     // System.out.println(second.toString());
     second.set(2,"set");
-    System.out.println(second.toString());
+    //System.out.println(second.toString());
     //second.set(2,null);
     second.add("Added");
-    System.out.println(second.toString());
+    //System.out.println(second.toString());
     //second.add(null);
     second.add(0,"One Half");
-    System.out.println(second.toString());
-    second.add(0,null);
+    ///System.out.println(second.toString());
+    //second.add(0,null);
+    third.add("A");
+    third.add("C");
+    third.add("D");
+    third.add("B");
+    System.out.println(third.toString());
   }
 }
